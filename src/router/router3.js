e61d9fe93f2a735c66166560c0b087a8b8f3510d
Arpitpurwar <31696497+Arpitpurwar@users.noreply.github.com>
2018-07-04 13:01:57 +0530

    File

--- src/router/router3.js
+++ src/router/router3.js
@@ -2,14 +2,29 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const file = JSON.parse(fs.readFileSync('../dashboardSummaryOM.json'));
-router.get('/teamSLA',(req,res)=>{
-
-   res.json(file);
+router.get('/SLA/:id',(req,res)=>{
+   if(req.params.id == "team"){
+    res.json(file);
+   }
+  else if(req.params.id == "application"){
+    res.json(file);
+   }
+  else{
+      res.send("wrong Url Guys");
+  }
 
 });
-router.get('/applicationSLA',(req,res)=>{
+router.get('/SLAComp/:id',(req,res)=>{
 
-    res.json(file);
+    if(req.params.id == "team"){
+        res.json(file);
+       }
+      else if(req.params.id == "application"){
+        res.json(file);
+       }
+      else{
+          res.send("wrong Url Guys");
+      }
  
  });
 module.exports=router;
