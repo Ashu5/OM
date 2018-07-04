b4b50dc9c468bb611da21efb09b551e20686bf36
Ashutosh Dwivedi <ashutd33@in.ibm.com>
2018-07-04 11:59:30 +0530

    updated files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-hello
+const express = require('express');
+const app = express();
+
+app.use('/api',require('./router/router1.js'));
+app.use('/api',require('./router/router1.js'));
+app.use('/api',require('./router/router1.js'));
+app.use('*',(req,res)=>{
+    res.send("wrong Url U entered");
+})
+
+app.listen(3000,(req,res)=>{"3000 is running"});
---
+++ src/router/router2.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+app.get('',(req,res)=>{
+
+
+
+
+
+
+});
+module.exports=router;
