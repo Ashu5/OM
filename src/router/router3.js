c50e6390b17164d2a6800b16df4f987b8c085184
Arpit Purwar <apurwa19@in.ibm.com>
2018-07-04 12:26:18 +0530

    fine

--- src/router/router3.js
+++ src/router/router3.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const file = JSON.parse(fs.readFileSync('../../dashboardSummaryOM.json'));
+const file = JSON.parse(fs.readFileSync('../dashboardSummaryOM.json'));
 router.get('/teamSLA',(req,res)=>{
 
    res.json(file);
