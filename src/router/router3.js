7bbe11e77fafc364a3ae9ef325d4aa7b988ae592
Arpitpurwar <31696497+Arpitpurwar@users.noreply.github.com>
2018-07-04 12:49:33 +0530

    commit

--- src/router/router3.js
+++ src/router/router3.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const file = JSON.parse(fs.readFileSync('../dashboardSummaryOM.json'));
 router.get('/teamSLA',(req,res)=>{
 
-   res.json(file+"fff");
+   res.json(file);
 
 });
 router.get('/applicationSLA',(req,res)=>{
