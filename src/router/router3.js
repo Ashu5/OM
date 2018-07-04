75aa8a055643ebb656d70301445f485ef91eff99
Arpitpurwar <31696497+Arpitpurwar@users.noreply.github.com>
2018-07-04 14:15:45 +0530

    Send Router3

--- src/router/router3.js
+++ src/router/router3.js
@@ -2,24 +2,54 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const file = JSON.parse(fs.readFileSync('../dashboardSummaryOM.json'));
+const package = JSON.parse(fs.readFileSync('../package.json'));
 router.get('/SLA/:id',(req,res)=>{
-   if(req.params.id == "team"){
-    res.json(file);
+   static = [package.variable.openApproach];
+    if(req.params.id == "team"){
+        var valu2=file[0].openApproachingSLAs.byPerson;
+          res.json(valu2);
    }
   else if(req.params.id == "application"){
-    res.json(file);
+    var valu2=file[0].openApproachingSLAs.byApp;
+    res.json(valu2);
    }
   else{
       res.send("wrong Url Guys");
   }
 
 });
-router.get('/SLAComp/:id',(req,res)=>{
+router.get('/SLAComp/team/:id',(req,res)=>{
 
-    if(req.params.id == "team"){
+    if(req.params.id == "weekly"){
         res.json(file);
        }
-      else if(req.params.id == "application"){
+      else if(req.params.id == "monthly"){
+        res.json(file);
+       }
+       else if(req.params.id == "quaterely"){
+        res.json(file);
+       }
+       else if(req.params.id == "yearly"){
+        res.json(file);
+       }
+      else{
+          res.send("wrong Url Guys");
+      }
+ 
+ });
+
+ router.get('/SLAComp/application/:id',(req,res)=>{
+
+    if(req.params.id == "weekly"){
+        res.json(file);
+       }
+      else if(req.params.id == "monthly"){
+        res.json(file);
+       }
+       else if(req.params.id == "quaterely"){
+        res.json(file);
+       }
+       else if(req.params.id == "yearly"){
         res.json(file);
        }
       else{
