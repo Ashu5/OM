2d252ae09b416f48e896501fdc806407a9ebc93a
Arpit Purwar <apurwa19@in.ibm.com>
2018-07-04 12:08:11 +0530

    Index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use('/api',require('./router/router1.js'));
-app.use('/api',require('./router/router1.js'));
-app.use('/api',require('./router/router1.js'));
+app.use('/api',require('./router/router2.js'));
+app.use('/api',require('./router/router3.js'));
 app.use('*',(req,res)=>{
     res.send("wrong Url U entered");
 })
