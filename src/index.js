8ac30a7517198d0ae9d42bf37dc6ede27aeb5373
Arpit Purwar <apurwa19@in.ibm.com>
2018-07-04 12:17:55 +0530

    index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.use('/api',require('./router/router1.js'));
 app.use('/api',require('./router/router2.js'));
 app.use('/api',require('./router/router3.js'));
 app.use('*',(req,res)=>{
-    res.send("wrong Url U entered");
+    res.send("wrong Url, Guys U entered");
 })
 
 app.listen(3000,(req,res)=>console.log("3000 is running"));
