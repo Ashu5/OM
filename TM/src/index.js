c5b2bbf88615bfeb0a2da143537de2198dd76a93
Arpitpurwar <31696497+Arpitpurwar@users.noreply.github.com>
2018-07-06 12:13:50 +0530

    team start

---
+++ TM/src/index.js
@@ -0,0 +1,20 @@
+// Require Express Dependency
+const express = require('express');
+const app = express();
+
+// For Dynamic port
+const fs = require('fs');
+const port = JSON.parse(fs.readFileSync('../package.json'));
+
+// Using Middleware
+app.use('/api',require('./router/router1.js'));
+app.use('/api',require('./router/router2.js'));
+app.use('/api',require('./router/router3.js'));
+
+// Default Url
+app.use('*',(req,res)=>{
+    res.send("wrong Url, Guys U entered");
+})
+
+//Listening  URl
+app.listen(port.variable.port,(req,res)=>console.log(port.variable.port +" is running"));
