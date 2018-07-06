8a7e4a970f14850032468d1e1becc2d7344c0c53
Ashutosh Dwivedi <ashutd33@in.ibm.com>
2018-07-06 16:30:01 +0530

    Router2 and Router1 completed for OT

--- TM/src/routes/route1.js
+++ TM/src/routes/route1.js
@@ -15,7 +15,7 @@ router.get('/raiseTM',(req,res)=>{
         var obj ={};
         
        if(key1[i]==static[i]){
-        objKey2[i]=file[0].escalations.my.totalCount;
+        objKey2[i]=file[0].escalations.my.totalCount;      
         
         }
         
@@ -29,38 +29,6 @@ router.get('/raiseTM',(req,res)=>{
     res.end();
 });
 
-// router.get('/coo',(req,res)=>{
-// res.setHeader('Content-Type', 'application/json');
-// var key1 = Object.keys(file[0].escalations.all);
-// result = [];
-// var obj1 ={};
-// var static=[external.variable.totalCount,external.variable.escCount];
-// for (var i = 0; i < key1.length; i++) 
-// {
-//    if(static.includes(key1[i]))
-//   {  obj1[key1[i]] = file[0]["escalations"]["all"]["totalCount"] }
-//  // console.log(obj);
-// }
-// result.push(obj1);
-
-// var key3 = Object.keys(file[0].escalations.all.byPriority);
-// var key2 = Object.keys(file[0].escalations.all.byPriority[0]);
-// for (var i = 0; i < key3.length; i++) 
-// {
-//    var obj = {};
-//    for(var j=0;j<key2.length;j++){
-//    if(static.includes(key2[j]))
-//   {  obj[key2[j]] = file[0]["escalations"]["all"]["byPriority"][i][key2[j]];
-//  }
- 
-// }
-// result.push(obj);
- 
-// }
-
-// res.json(result);
-// res.end();
-// });
 
 module.exports = router;
 
