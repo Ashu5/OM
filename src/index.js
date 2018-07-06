bdbfa00cf713b61912f3816ce6dd379ff916b5e1
Arpitpurwar <31696497+Arpitpurwar@users.noreply.github.com>
2018-07-06 11:24:46 +0530

    Final Chnages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+// Require Express Dependency
 const express = require('express');
 const app = express();
 
+// For Dynamic port
+const fs = require('fs');
+const port = JSON.parse(fs.readFileSync('../package.json'));
+
+// Using Middleware
 app.use('/api',require('./router/router1.js'));
 app.use('/api',require('./router/router2.js'));
 app.use('/api',require('./router/router3.js'));
+
+// Default Url
 app.use('*',(req,res)=>{
     res.send("wrong Url, Guys U entered");
 })
 
-app.listen(3000,(req,res)=>console.log("3000 is running"));
+//Listening  URl
+app.listen(port.variable.port,(req,res)=>console.log(port.variable.port +" is running"));
